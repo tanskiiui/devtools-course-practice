37ef740708885f7e96e177a857ddb2fb25e973ae
Alice105973 <kukushkinako13081999@gmail.com>
2020-05-06 17:33:05 +0300

    lint

--- modules/statistics/test/test_statistics_app.cpp
+++ modules/statistics/test/test_statistics_app.cpp
@@ -15,7 +15,7 @@ using std::vector;
 using std::string;
 
 class Statistics_app_test : public ::testing::Test {
-protected:
+ protected:
   void Act(vector<string> args_) {
     vector<const char*> options;
 
@@ -34,7 +34,7 @@ protected:
     EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
   }
 
-private:
+ private:
   Statistics_app app_;
   string output_;
 };
