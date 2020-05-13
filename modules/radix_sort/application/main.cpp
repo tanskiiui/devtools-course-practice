acc1d7b52508480b47e20317d9b38030755ec650
Alex Frog <aleksandr.antipin.1999@mail.ru>
2020-05-13 03:01:37 +0300

    2-rd lab

---
+++ modules/radix_sort/application/main.cpp
@@ -0,0 +1,8 @@
+#include "..\include\radix_sort_app.h"
+#include "iostream"
+
+int main(int argc, const char** argv) {
+    SortVectorApp app;
+    std::cout << app(argc, argv) << std::endl;
+    return 0;
+}
---
+++ modules/radix_sort/include/radix_sort_app.h
@@ -0,0 +1,22 @@
+// Copyright 2020 Antipin Alexander
+
+#ifndef MODULES_RADIX_SORT_INCLUDE_RADIX_SORT_APP_H_
+#define MODULES_RADIX_SORT_INCLUDE_RADIX_SORT_APP_H_
+
+#include <string>
+#include <sstream>
+#include "include\radix_sort.h"
+
+class SortVectorApp {
+ public:
+    SortVectorApp();
+    std::string operator()(int argc, const char** argv);
+ private:
+    std::string help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+ private:
+    SortArray _array;
+    std::stringstream _sstream;
+};
+
+#endif  // MODULES_RADIX_SORT_INCLUDE_RADIX_SORT_APP_H_
