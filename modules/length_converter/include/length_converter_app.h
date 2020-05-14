649ca16bdfb1a750f81c5e4a17c9db66340c925d
Anorak52 <siber56@yandex.ru>
2020-05-14 22:17:12 +0300

    First version without tests fix travis

--- modules/length_converter/include/length_converter_app.h
+++ modules/length_converter/include/length_converter_app.h
@@ -6,21 +6,19 @@
 #include <string>
 #include "include/length_converter.h"
 
-using namespace std;
-
 class LengthCalculator {
-public:
+ public:
     LengthCalculator() = default;
     std::string operator()(int argc, const char** argv);
-private:
+ private:
     void Help(const char* appname, const char* message = "");
     bool ValidateNumberOfArgs(int argc, const char** argv);
     std::string message_;
     typedef struct {
         double value;
-        string operation;
+        std::string operation;
         int side;
     } Arguments;
 };
 
-#endif  // MODULES_CALCULATOR_COMPLEX_NUMS_INCLUDE_LENGTH_CONVERTER_APP_H_
+#endif  // MODULES_LENGTH_CONVERTER_INCLUDE_LENGTH_CONVERTER_APP_H_
